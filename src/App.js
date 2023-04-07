60679e0220d847c352661e692bebc7c36c11290d
jorlar <jqern.larsen@gmail.com>
2023-04-07 22:03:51 +0200

    :sparkles: Refactor code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import Header from "./ components/Header";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Header from './components/Header'
+import FeedbackList from './components/FeedbackList'
+import FeedbackStats from './components/FeedbackStats'
+import FeedbackForm from './components/FeedbackForm'
+import AboutIconLink from './components/AboutIconLink'
+import AboutPage from './pages/AboutPage'
+import { FeedbackProvider } from './context/FeedbackContext'
 
 function App() {
   return (
-    <>
-      <Header />
-      <div className='container'>
-        <h1>My App</h1>
-      </div>
-    </>
-  );
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <>
+                  <FeedbackForm />
+                  <FeedbackStats />
+                  <FeedbackList />
+                </>
+              }
+            ></Route>
+
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+
+          <AboutIconLink />
+        </div>
+      </Router>
+    </FeedbackProvider>
+  )
 }
 
-export default App;
+export default App
